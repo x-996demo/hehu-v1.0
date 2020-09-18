9d8dce0cb12006869319b333c9fbd71380cb6dca
xsl <x976403038@163.com>
2020-09-18 08:39:09 +0800

    引入并注册elementui

--- src/main.js
+++ src/main.js
@@ -1,9 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
+import ElementUI from 'element-ui' // 引入elementui
+import 'element-ui/lib/theme-chalk/index.css' // 引入elementui样式
 import router from './router'
 
 Vue.config.productionTip = false
-
+Vue.use(ElementUI) // 全局注册使用elementui
 new Vue({
   router,
   render: h => h(App)
